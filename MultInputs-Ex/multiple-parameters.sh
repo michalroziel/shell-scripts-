910fa438ba8c582e3c0a376a10ee41d8c32436c3
Michał Roziel <rozielmichal@gmail.com>
2024-02-26 22:01:23 +0100

    added an Ping example and a multi parameter example

---
+++ MultInputs-Ex/multiple-parameters.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+#
+#		this script takes multiple parameters and prints them to the command line 
+
+	if [ "$#" -lt 2  ]
+		then 
+		echo " Usage : $0 <param1> <param2> [param3...]	"
+		exit 1 
+
+	fi 
+
+
+	# output all parameters to the command line 
+
+	echo " Parameters given :  "
+
+	for param in "$@"; do 
+
+		echo "$param"
+
+	done 
+
+
+	# Print the number of parameters 
+
+
+	echo " The number of parameters provided is : "$#" " 
